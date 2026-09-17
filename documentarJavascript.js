85a0c7fc54825690709383192d7541acfe69dc99
yculquivil <yculquivil@educacion.navarra.es>
2026-09-17 09:44:58 +0200

    gitinore

---
+++ documentarJavascript.js
@@ -0,0 +1,48 @@
+/**
+ *   Autor=TU_NOMBRE (debe aparecer tu nombre)
+ *   Fecha=15-sep-2026
+ *   Licencia=GPL v3
+ *   Version=1.0
+ *   Descripción=Descripción general del contenido de este script. Por ejemplo,
+ *   script para validaciones de entrada de datos del usuario. NIF, números, texto...
+ **/
+
+/*
+ * Copyright (C) 2026 TU_NOMBRE (debe aparece el año en el que se cree y tu nombre)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//Cuando el nombre de la variable no sea representativo de lo que almacena, añade un comentario explicativo.
+var variable;
+
+//Comentarios explicativos del código siempre que sea necesario
+variable = prompt('...');
+//...
+
+/**
+ * descripción general de lo que hace la función
+ * descripción de argumentos de entrada
+ */
+function nombre(argumento1, argumento2) {
+  //Cuando el nombre de la variable no sea representativo de lo que almacena, añade un comentario explicativo.
+  var variable1;
+
+  //Comentarios explicativos del código siempre que sea necesario
+  for (i = 0; i < n; i++) {
+    //...
+  }
+
+  return valor;
+}
